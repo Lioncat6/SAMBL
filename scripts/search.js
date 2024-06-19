6b652e0be8c40e9b878b30994fb29ccbd2ea3c8a
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-06-19 17:42:08 -0500

    Initial Commit

---
+++ scripts/search.js
@@ -0,0 +1,41 @@
+
+function lookup() {
+    document.getElementById("err").innerHTML=" "
+    document.getElementById("searchEnter").innerHTML="<div class=\"lds-ellipsis\"><div></div><div></div><div></div><div></div></div>"
+    const query = document.getElementById('searchbox').value;
+    if (query != "") {
+        const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+        if(query.includes("https://open.spotify.com/artist/")) {
+            const match = spotifyUrl.match(/\/artist\/([^/?]+)/);
+            if (match) {
+                const spfId  = match[1];
+                
+            } else {
+                
+            }
+        } else if (uuidPattern.test(query)) {
+
+        } else {
+            invalidInput("aaa")
+        }
+    } else {
+        invalidInput("Please enter a query")
+    }
+}
+
+function invalidInput(reason){
+    document.getElementById("err").innerHTML=reason
+    document.getElementById("searchEnter").innerHTML="Search"
+}
+
+function fetchSpotifyArtist(data) {
+
+}
+
+function fetchMBArtist(data) {
+
+}
+
+function spotifySearch(data) {
+
+}
