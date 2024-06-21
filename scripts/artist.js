7b25ff6896d9d2f67f735b086d6c72285191b7ec
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-06-21 05:49:59 -0500

    artist page test

---
+++ scripts/artist.js
@@ -0,0 +1,47 @@
+function dispErr(error) {
+    document.getElementById("err").innerHTML = error
+}
+
+async function fetchSpotifyArtist(artist) {
+    var fsatoken = localStorage.getItem("spfAccessToken") 
+    const response = await fetch('https://api.spotify.com/v1/artists/' + artist, {
+      headers: {
+        Authorization: 'Bearer ' + fsatoken
+      }
+    });
+  
+    const data = await response.json();
+    console.log(data)
+    if (!data["error"]) {
+        const imgUrl = data["images"][0]
+        document.getElementById("artistImageContainer").innerHTML="<a href=\""+imgurl+"\" target=\"_blank\"><img src=\""+imgurl+"\"></a>"
+    } else {
+        if (data["error"]["status"] == 404) {
+            dispErr("Spotify artist not found!")
+        } else if (data["error"]["status"] == 400) {
+            dispErr("Invalid artist id!")
+        } else {
+            fsatoken = localStorage.getItem("spfAccessToken") 
+            console.log(fsatoken)
+            if (!fsatoken) {
+                fsatoken = "";
+            }
+            if (fsatoken & fsatoken.length > 10) {
+                linkSpotify()
+                dispErr("Spotify Timeout | Please reload")
+            }
+        }
+        
+    }
+}
+
+async function downloadSpotifyAlbums (artist) {
+
+
+}
+
+const params = new URLSearchParams(new URL(window.location.href).search);
+const spid = params.get("spid");
+const mbid = params.get("mbid");
+
+fetchSpotifyArtist(spid)
