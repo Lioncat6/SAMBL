d4b6a971ab13eee108a8161f1f1f3389203bb36d
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-06-27 09:52:03 -0500

    Create newartist.js

---
+++ scripts/newartist.js
@@ -0,0 +1,54 @@
+function dispErr(error) {
+    document.getElementById("err").innerHTML = error
+}
+
+async function fetchSpotifyArtist(artist) {
+    var fsatoken = localStorage.getItem("spfAccessToken") 
+    const response = await fetch('https://api.spotify.com/v1/artists/' + artist, {
+      headers: {
+        Authorization: 'Bearer ' + fsatoken
+      }
+    });
+  
+    const data = await response.json();
+    console.log(data)
+    if (!data["error"]) {
+        const spImgUrl = data["images"][0]["url"]
+        const spArtistName = data["name"]
+        const spArtistUrl = data["external_urls"]["spotify"]
+        console.log(spArtistName)
+        console.log(spArtistUrl)
+        console.log(spImgUrl)
+        document.getElementById("artistImageContainer").innerHTML="<a href=\""+spImgUrl+"\" target=\"_blank\"><img src=\""+spImgUrl+"\"></a>"
+        document.getElementById("spURL").setAttribute("href", spArtistUrl);
+        document.getElementById("artistName").innerHTML=spArtistName
+        document.getElementsByTagName("title")[0].innerHTML="SAMBL • "+spArtistName
+    } else {
+        if (data["error"]["status"] == 404) {
+            dispErr("Spotify artist not found!")
+        } else if (data["error"]["status"] == 400) {
+            dispErr("Invalid artist id!")
+        } else {
+            fsatoken = localStorage.getItem("spfAccessToken") 
+            console.log(fsatoken)
+            if (!fsatoken) {
+                fsatoken = "";
+            }
+            if (fsatoken & fsatoken.length > 10) {
+                linkSpotify()
+                dispErr("Spotify Timeout | Please reload")
+                location.reload()
+            }
+        }
+        
+    }
+}
+
+const params = new URLSearchParams(new URL(window.location.href).search);
+const spid = params.get("spid");
+
+if ((spid)) {
+    fetchSpotifyArtist(spid)
+} else {
+    dispErr("Incomplete Url!")
+} 
