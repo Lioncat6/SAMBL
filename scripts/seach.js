d88286490c9ed51a7cfd91319bd395545204b77f
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-10-02 21:59:42 -0500

    this code sucks lol

---
+++ scripts/seach.js
@@ -0,0 +1,116 @@
+let apiUrl = "https://s-api.lioncat6.com:20683";
+
+function dispErr(error) {
+	document.getElementById("err").innerHTML = error;
+}
+
+async function searchSpotify(query) {
+	var fsatoken = localStorage.getItem("spfAccessToken");
+	const response = await fetch(`${apiUrl}/v1/search?q=` + query + "&type=artist&limit=20", {
+		headers: {
+			Authorization: "Bearer " + fsatoken,
+		},
+	});
+
+	const data = await response.json();
+	console.log(data);
+	if (!data["error"]) {
+		for (x in data["items"]) {
+			spotifyArtistList.push(data["items"][x]);
+		}
+	} else {
+		if (data["error"]["status"] == 404) {
+			dispErr("Spotify artist not found!");
+		} else if (data["error"]["status"] == 400) {
+			dispErr("Invalid artist id!");
+		} else {
+			fsatoken = localStorage.getItem("spfAccessToken");
+			console.log(fsatoken);
+			if (!fsatoken) {
+				fsatoken = "";
+			}
+			if (fsatoken && fsatoken.length > 10) {
+				dispErr("Spotify Timeout | Please reload");
+				//location.reload()
+			}
+		}
+	}
+	await processArtists();
+}
+
+function capFirst(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+async function fetchMBArtist(id) {
+	const response = await fetch("https://musicbrainz.org/ws/2/url?limit=1&inc=artist-rels+label-rels+release-rels&fmt=json&resource=https://open.spotify.com/artist/" + id);
+	const data = await response.json();
+	if (response.status == 200) {
+		const mbid = data["relations"][0]["artist"]["id"];
+		return "https://lioncat6.github.io/SAMBL/artist?spid=" + id + "&artist_mbid=" + mbid;
+	} else if ((data["error"] = "Not Found" || response.status == 404)) {
+		return "https://lioncat6.github.io/SAMBL/newartist?spid=" + id;
+	} else {
+		console.log("MusicBrainz Error: " + data["error"]);
+		return null;
+	}
+}
+
+async function processArtists() {
+	var total = 0;
+	displayList();
+	for (x in spotifyArtistList) {
+		var currentArtist = spotifyArtistList[x];
+		var spotifyUrl = currentArtist["external_urls"]["spotify"];
+		var spotifyId = currentArtist["id"];
+		var spotifyName = currentArtist["name"];
+		var spotifyImageURL = currentArtist["images"][0]["url"];
+		var spotifyFollowers = currentArtist["followers"]["total"];
+		const spGenres = currentArtist["genres"];
+		var spGenresString = "";
+		for (x in spGenres) {
+			if (x > 0) {
+				spGenresString += ", ";
+			}
+			spGenresString += spGenres[x];
+		}
+		total++;
+
+		var htmlToAppend =
+			'<div class="album listItem"><a href="' +
+			await fetchMBArtist(spotifyId) +
+			'"><div class="artistIcon"><a href="' +
+			spotifyImageURL +
+			'" target="_blank"><img src="' +
+			spotifyImageURL +
+			'" /></a></div><div class="textContainer"><div class="artistName"><a href="' +
+			spotifyUrl +
+			'" target="_blank" >' +
+			spotifyName +
+			"</a>" +
+			'</div><div class="artistInfo">' +
+			spotifyFollowers +
+			" Followers" +
+			'</div><div class="artistGenres">' +
+			spGenresString +
+			"</div></div></div>";
+		var htmlObject = document.createElement("div");
+		htmlObject.innerHTML = htmlToAppend;
+		document.getElementById("albumList").append(htmlObject);
+	}
+}
+
+function displayList() {
+	document.getElementById("artistContainer").style.display = "flex";
+}
+
+function addListItem() {}
+
+const params = new URLSearchParams(new URL(window.location.href).search);
+const query = params.get("query");
+if (!query) {
+	dispErr("Malformed url; Lacking a query!");
+} else {
+    document.getElementById("searchFor").innerHTML="Search for \""+query+"\""
+    searchSpotify(query)
+}
