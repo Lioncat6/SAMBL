db4abd18df66607b0fe19b583a4fc53ee5a2d483
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-11-03 22:57:29 -0600

    *sigh*

--- scripts/artist.js
+++ scripts/artist.js
@@ -68,7 +68,7 @@ async function fetchSpotifyArtists(artists) {
 	const spotifyIconsHtml = allUrls.map(url => 
 	  `<a href="${url}" target="_blank"><img src="../assets/images/Spotify_icon.svg" alt="Spotify" class="spIcon"></a>`
 	).join('');
-	document.getElementByClassName("spURLContainer")[0].innerHTML = spotifyIconsHtml;
+	document.getElementsByClassName("spURLContainer")[0].innerHTML = spotifyIconsHtml;
   }
 
 async function fetchSpotifyArtist(artist) {
