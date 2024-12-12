96210945f5c535e9406920ee51918a285fa81f1e
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-12-12 14:36:28 -0600

    tweak dragging code

--- scripts/artist.js
+++ scripts/artist.js
@@ -509,9 +509,9 @@ function closeFilter() {
 
 function dragElement(elmnt) { //from w3 schools
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.getElementById(elmnt.id + "Header")) {
     /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
   } else {
     /* otherwise, move the DIV from anywhere inside the DIV:*/
     elmnt.onmousedown = dragMouseDown;
