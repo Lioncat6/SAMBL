0f376b35be5127ee6bb5d6ee5b568f7ac6400a2a
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-12-16 02:55:00 -0600

    baldi's basics in education and learning

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,21 +4,18 @@ let apiUrl = "https://s-api.lioncat6.com:20683";
 function checkServerStatus() {
     fetch(`${apiUrl}/uptime`)
         .then(response => {
-            if (!response.ok) { // Check if response status is not ok
-                if (response.status == 503) {
-                    statusElement.innerHTML = `⚠️ Server failed to connect to Spotify`;
-                    statusElement.className = 'offline';
-                } else {
-                    statusElement.innerHTML = `⚠️ Unknown Server Error`;
-                    statusElement.className = 'offline';
-                }
-                throw new Error(`HTTP error! status: ${response.status}`);
+            if (response.status === 200) {
+                return response.json().then(data => {
+                    statusElement.innerHTML = `Server Online: ${data.human_readable}`;
+                    statusElement.className = 'online';
+                });
+            } else if (response.status === 503) {
+                statusElement.innerHTML = `⚠️ Server failed to connect to Spotify`;
+                statusElement.className = 'offline';
+            } else {
+                statusElement.innerHTML = `⚠️ Unknown Server Error`;
+                statusElement.className = 'offline';
             }
-            return response.json();
-        })
-        .then(data => {
-            statusElement.innerHTML = `Server Online: ${data.human_readable}`;
-            statusElement.className = 'online';
         })
         .catch(() => {
             statusElement.innerHTML = '⚠️ Server Unreachable';
