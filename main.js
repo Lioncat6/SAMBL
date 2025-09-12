2be5c54dc275fd5266f0dfab47fbc59e4b6481d7
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2025-09-11 21:33:21 -0500

    Update

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-const newUrl = 'http://sambl.lioncat6.com' + window.location.search + window.location.hash;
+const newUrl = 'http://sambl.lioncat6.com' + window.location.pathname.replace('/SAMBL', '') + window.location.search + window.location.hash;
 window.location.replace(newUrl);
